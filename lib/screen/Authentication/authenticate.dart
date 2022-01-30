8916d409afc7fd0029e28cb23c5df38fac19069b
Nipun Hedaoo <nipunhedaoo@gmail.com>
2022-01-30 10:34:29 +0530

    basic setup

---
+++ lib/screen/Authentication/authenticate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  const ({ Key? key }) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Authentication Sceen'),
+    );
+  }
+}
---
+++ lib/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:freemeals/screen/Home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  //  Either home or authenticate
+    return Home();
+  }
+}
